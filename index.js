4e89a40012bb439d94e2599e45fc68348c83821a
Cleef Souza <a.cleef.souza@gmail.com>
2020-06-01 21:30:30 -0300

    Alterando img dinamicamente

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const key = 'AAD_SUA_KEY';
+const key = '6bc88373';
 const format = 'json-cors';
 const urlBase = 'https://api.hgbrasil.com/weather?';
 const fields =
   'array_limit=8&fields=only_results,city,description,date,temp,forecast,weekday,max';
 
 const days = {
-  Dom: 'Domingo',
-  Seg: 'Segunda',
-  Ter: 'Terça',
-  Qua: 'Quarta',
-  Qui: 'Quinta',
-  Sex: 'Sexta',
-  Sáb: 'Sábado',
+  "Dom": 'Domingo',
+  "Seg": 'Segunda',
+  "Ter": 'Terça',
+  "Qua": 'Quarta',
+  "Qui": 'Quinta',
+  "Sex": 'Sexta',
+  "Sáb": 'Sábado',
 };
+
+const pathImg = {
+  spring: '../assets/images/spring-day.jpg',
+  winter: '../assets/images/winter-day.jpg',
+  sunset: '../assets/images/sunset-day.jpg'
+}
 
 const getUrlParams = url => new URLSearchParams(url).get('search');
 
@@ -58,6 +64,8 @@ function setToday(json) {
   $('#hour').text(currentTime());
   $('#today').text(days[json.weekday]);
   $('#description').text(json.description);
+
+  changeBackground(json.temp);
 }
 
 function renderElementsWeekdays(json) {
@@ -82,5 +90,23 @@ const createElement = (day, temp) => {
 
   return divDayWeek;
 };
+
+function changeBackground(temp) {
+  const props =  'no-repeat center center / cover';
+
+    if (temp >= 30) {
+      $('.cl-panel').css({
+        'background' : `url(${pathImg.sunset}) ${props}`
+        });
+    } else if (temp <= 22) {
+      $('.cl-panel').css({
+        'background' : `url(${pathImg.winter}) ${props}`
+        });
+    } else {
+      $('.cl-panel').css({
+        'background' : `url(${pathImg.spring}) ${props}`
+        });
+    }
+}
 
 getForCity();
